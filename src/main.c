9ef73708690e0714bb0468c309c83c605e30be98
Gurvir <GurvirSD@Gmail.com>
2026-09-17 03:07:21 -0700

    Validate encryption commands

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Error: incorrect number of arguments\nUsage: ./filecrypt <command> <filename>\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "encrypt") != 0 && 
+        strcmp(argv[1], "decrypt") != 0) {
+        fprintf(stderr, "Error: unknown command '%s'\nUsage: ./filecrypt <command> <filename>\n", argv[1]);
         return 1;
     }
 
