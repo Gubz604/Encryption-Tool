546ae439b686d2d9440d5ada3dbdc04a004b6d3f
Gurvir <GurvirSD@Gmail.com>
2026-09-22 03:06:40 -0700

    Separate file I/O into module

---
+++ src/file_io.h
@@ -0,0 +1,8 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <stdio.h>
+
+int copy_file_data(FILE *input, FILE *output);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int copy_file_data(FILE *input, FILE *output);
+#include "file_io.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -66,43 +66,4 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     return 0;
-}
-
-int copy_file_data(FILE *input, FILE *output) {
-    unsigned char buffer[1024];
-    size_t total_bytes = 0;
-
-    while(1) {
-        size_t bytes_read = fread(buffer, 1, sizeof(buffer), input);
-
-        if (bytes_read > 0) {
-            printf("Read: %zu bytes\n", bytes_read);
-            total_bytes += bytes_read;
-
-            size_t output_produced = fwrite(buffer, 1, bytes_read, output);
-        
-            if (output_produced != bytes_read) {
-                fprintf(stderr, "Error: bytes written different than read\nBytes Read: %zu\nBytes written %zu\n", 
-                        bytes_read, output_produced);
-
-                return 0;
-            }
-        }
-
-        if (bytes_read < sizeof(buffer)) {
-            if (ferror(input)) {
-                fprintf(stderr, "Error during reading occurred\n");
-
-                return 0;
-            }
-
-            if (feof(input)) {
-                printf("End of file reached\n");
-                break;
-            }
-        }
-    }
-
-    printf("Total bytes read: %zu\n", total_bytes);
-    return 1;
 }
